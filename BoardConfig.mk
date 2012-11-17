b99342d34278377da94cf056186566a21ee660f8
dhacker29 <davidhackerdvm@gmail.com>
2012-11-16 20:44:27 -0600

    Updates for 4.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,11 @@
 # inherit from the proprietary version
 -include vendor/motorola/vanquish/BoardConfigVendor.mk
 
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11811160064
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/motorola/vanquish/bluetooth
+
 # Kernel
 #TARGET_PREBUILT_KERNEL := device/motorola/vanquish/kernel
 
